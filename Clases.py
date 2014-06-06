d08ee1e48bd7327147b202dfb9de06d5ba26e664
Catalina Libuy <cata.libuy@gmail.com>
2014-06-06 18:21:26 -0400

    Definición de clases
    Descripción de las clases principales del juego

---
+++ Clases.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Clases para el juego
+
+# LUGAR: Isla en la que pueden haber pasajeros
+
+# BOTE: Transporta un límite de pasajeros de un lugar a otro, necesita un humano para funcionar
+
+# BOTERO: Humano encargado de transportar pasajeros y cuidar que no se coman mutuamente
+
+# PASAJERO: Un animal o vegetal que come a otro pasajero si es de un nivel inmediatamente inferior
+
+# JUGADA: Movimiento de un turno
+
+# JUEGO: Juego completo compuesto de varias jugadas y un resultado
+
